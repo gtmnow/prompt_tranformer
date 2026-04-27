bf3035be6b990889b82191d4e8339897c2b8512c
Michael Anderson <mike@gtmnow.net>
2026-04-27 13:08:35 -0400

    Merge Alembic heads for Railway startup

---
+++ alembic/versions/20260427_0005_merge_heads.py
@@ -0,0 +1,14 @@
+"""merge alembic heads after request log rename and llm score columns"""
+
+revision = "20260427_0005"
+down_revision = ("20260418_0004", "20260427_0004")
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
